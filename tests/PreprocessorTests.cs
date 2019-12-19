0a183ec1bc3604a915d97c5cee94300de9a69947
Josh Wyant <joshwyant91@gmail.com>
2019-12-18 18:47:04 -0800

    Adding a test for nested includes.

--- tests/PreprocessorTests.cs
+++ tests/PreprocessorTests.cs
@@ -73,5 +73,40 @@ namespace tests
                     "end-of-file"
                 }, tokens.Select(t => t.ToString()));
         }
+
+        [Fact]
+        public async void TestNestedIncludes()
+        {
+            var program = 
+                @"#include ""a.h""
+                #include ""b.h""
+                int main() {
+                    printf(""Hello, world!\n"");
+                }";
+            var preprocessor = CreateTestPreprocessor(program, true);
+            var resolver = preprocessor.TranslationUnit.FileResolver as FakeFileResolver;
+            resolver.DefineFile("a.h", @"int someFuncA();");
+            resolver.DefineFile("b.h", "int someFuncB();\n#include \"c.h\"");
+            resolver.DefineFile("c.h", "int someFuncC();");
+            var tokens = (await preprocessor.AsList());
+            Assert.Equal(new[] { 
+                    Int, Identifier, LeftParen, RightParen, Semicolon,
+                    Int, Identifier, LeftParen, RightParen, Semicolon,
+                    Int, Identifier, LeftParen, RightParen, Semicolon,
+                    Int, Identifier, LeftParen, RightParen, LeftBrace,
+                    Identifier, LeftParen, StringLiteral, RightParen, Semicolon,
+                    RightBrace,
+                    Eof
+                }, tokens.Terminals());
+            Assert.Equal(new[] { 
+                    "int", "someFuncA", "(", ")", ";",
+                    "int", "someFuncB", "(", ")", ";",
+                    "int", "someFuncC", "(", ")", ";",
+                    "int", "main", "(", ")", "{",
+                    "printf", "(", "Hello, world!\n", ")", ";",
+                    "}",
+                    "end-of-file"
+                }, tokens.Select(t => t.ToString()));
+        }
     }
 }
