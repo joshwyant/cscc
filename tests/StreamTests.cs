0839ed6fbc672f043a3dcca82e7b34a890d71f1d
Josh Wyant <joshwyant91@gmail.com>
2020-05-13 21:49:05 -0700

    Removing stray Task.Delay() from a test

--- tests/StreamTests.cs
+++ tests/StreamTests.cs
@@ -110,7 +110,6 @@ namespace tests
         {
             for (var i = 0; i < 10; i++)
             {
-                await Task.Delay(100);
                 yield return i;
             }
         }
